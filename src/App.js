50a530520f68c18a88ccf3411b7a974083285e2b
Lliam McBean <lliam.mcbean@gmail.com>
2022-01-25 11:02:45 -0700

    commiting to move to shaders

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import { Canvas, extend } from '@react-three/fiber'
-import { shaderMaterial } from '@react-three/drei'
-import glsl from 'babel-plugin-glsl/macro'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Suspense } from 'react'
 
-const WaveShaderMaterial = new shaderMaterial(
-  {},
-  // vertex shader
-  glsl`
-    void main() {
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-  `,
-  // fragment shader
-  glsl`
-    void main() {
-      gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
-    }
-  `
-)
-
-extend({WaveShaderMaterial})
 
 function App() {
+  const gltf = useLoader(GLTFLoader, './untitled.glb')
   return (
     <div style={{height: '100vh', width: '100vw'}}>
         <Canvas>
-          <pointLight position={[10,10,10]} />
-          <mesh>
-            <planeBufferGeometry args={[3,5]} />
-            <waveShaderMaterial />
-          </mesh>
+          <Suspense fallback={null}>
+            <primitive object={gltf.scene} />
+          </Suspense>
         </Canvas>
       </div>
   );
