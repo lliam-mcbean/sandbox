61fd65c4a0054acde4376ac46f4045c7cbd4a9e6
Lliam McBean <lliam.mcbean@gmail.com>
2022-02-23 14:33:16 -0700

    plane rotation on mouse move

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import { Canvas } from '@react-three/fiber'
-import { Physics, usePlane, useBox } from '@react-three/cannon'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Physics, usePlane, useSphere } from '@react-three/cannon'
+import { OrbitControls } from '@react-three/drei'
+import { useState, useRef } from 'react'
 
 function Plane(props) {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
+  const [ref, api] = usePlane(() => ({ rotation: props.rotation, ...props }))
+  console.log(props.rotation)
+  useFrame(({mouse}) => {
+    if(props.isActive) {
+      api.rotation.set(-Math.PI / 2 - mouse.y, 0, 0)
+    }
+    
+  })
   return (
     <mesh ref={ref}>
       <planeGeometry args={[100, 100]} />
@@ -11,11 +20,26 @@ function Plane(props) {
 }
 
 function Cube(props) {
-  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], ...props }))
+  const [ref, api] = useSphere(() => ({ mass: 1, position: [0, 5, 0], ...props }))
+  
   return (
     <mesh ref={ref}>
-      <boxGeometry />
+      <sphereGeometry />
+      <meshNormalMaterial />
     </mesh>
+  )
+}
+
+function InnerCube() {
+  const [rotation, setRotation] = useState([-Math.PI / 2, 0, 0])
+  
+  return (
+    <>
+    <group>
+      <Plane rotation={rotation} isActive={true}/>
+      <Plane rotation={[0,0,0]} position={[0,25,-25]} isActive={false}/>
+    </group>
+    </>
   )
 }
 
@@ -23,10 +47,12 @@ function App() {
   return (
     <div style={{height: '100vh', width: '100vw'}}>
         <Canvas>
+          <ambientLight />
           <Physics>
-            <Plane />
+            <InnerCube />
             <Cube />
           </Physics>
+          <OrbitControls />
         </Canvas>
       </div>
   );
